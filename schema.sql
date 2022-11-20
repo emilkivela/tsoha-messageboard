9b38ebe028c6888be136c00b1df7acd7a88b5705
Emil Kivelä <emil.kivela@gmail.com>
2022-11-20 17:00:32 +0200

    added SQL schema

---
+++ schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    admin BOOLEAN
+);
+
+CREATE TABLE threads(
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    creator INTEGER REFERENCES users,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE messages(
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    thread INTEGER REFERENCES threads,
+    author INTEGER REFERENCES users,
+    sent_at TIMESTAMP
+);
